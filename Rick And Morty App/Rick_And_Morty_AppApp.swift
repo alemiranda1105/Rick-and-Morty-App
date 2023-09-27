743a410fcd2c844f2737c2da66ea6939ead4d217
Alejandro Miranda <amiranda110500@gmail.com>
2023-09-27 14:45:34 +0100

    Initial Commit

---
+++ Rick And Morty App/Rick_And_Morty_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_And_Morty_AppApp.swift
+//  Rick And Morty App
+//
+//  Created by Alejandro Miranda on 27/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Rick_And_Morty_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
